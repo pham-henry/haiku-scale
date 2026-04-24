aaeb5b09abd2893d67c32f07661b673b52d3c3f2
Henry Pham <henry.pham323@gmail.com>
2026-04-23 21:28:49 -0700

    included wifi header

--- embedded/haiku_tea_scale/secrets.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-constexpr const char* WIFI_SSID     = "YOUR_SSID";
-constexpr const char* WIFI_PASSWORD = "YOUR_PASSWORD";
-
-// Change to Tailscale IP
-constexpr const char* SERVER_URL    = "http://192.168.1.50:3000/api/tea-weight";
